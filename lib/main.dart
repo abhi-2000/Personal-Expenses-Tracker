6935e4f429fc17697ad28f060a236f90f30bbbb3
abhi-2000 <abhisheksangwan72@gmail.com>
2021-08-27 00:44:27 +0530

    Expenses History designing

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import './transaction.dart';
-
+import 'package:intl/intl.dart';
 void main() {
   runApp(MyApp());
 }
@@ -38,7 +38,7 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text('Flutter App'),
+          title: Text('Expenses Tracker'),
         ),
         body: Column(
           children: <Widget>[
@@ -58,14 +58,32 @@ class MyHomePage extends StatelessWidget {
                   child: Row(
                     children: [
                       Container(
+                        margin:
+                            EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.purple, width: 2),
+                        ),
+                        padding: EdgeInsets.all(10),
                         child: Text(
-                          tx.amount.toString(),
+                          '₹ ${tx.amount}',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15,
+                              color: Colors.purple),
                         ),
                       ),
                       Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Text(tx.title),
-                          Text(tx.date.toString()),
+                          Text(
+                            tx.title,
+                            style: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            DateFormat.yMMMd().format(tx.date),
+                            style: TextStyle(color: Colors.grey),
+                          ),
                         ],
                       )
                     ],
