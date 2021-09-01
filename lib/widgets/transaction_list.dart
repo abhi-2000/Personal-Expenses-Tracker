edf9557476ffb010c7b75f52a013bc5e5bf30558
abhi-2000 <abhisheksangwan72@gmail.com>
2021-09-02 01:28:34 +0530

    landscape ui change

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -13,56 +13,74 @@ class Transaction_list extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return transaction.isEmpty
-        ? LayoutBuilder(builder: (ctx,constraints){
-          return  Column(
-            children: [
-              Text(
-                'No transactions added yet!',
-                style: Theme.of(context).textTheme.title,
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Container(
-                  height: constraints.maxHeight*0.6,
-                  child: Image.asset(
-                    'assets/images/waiting.png',
-                    fit: BoxFit.cover,
-                  )),
-            ],
-          );
-    }
-
-        )
+        ? LayoutBuilder(builder: (ctx, constraints) {
+      return Column(
+        children: [
+          Text(
+            'No transactions added yet!',
+            style: Theme
+                .of(context)
+                .textTheme
+                .title,
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Container(
+              height: constraints.maxHeight * 0.6,
+              child: Image.asset(
+                'assets/images/waiting.png',
+                fit: BoxFit.cover,
+              )),
+        ],
+      );
+    })
         : ListView.builder(
-            itemBuilder: (ctx, index) {
-              return Card(
-                elevation: 2,
-                margin: EdgeInsets.symmetric(vertical: 7, horizontal: 10),
-                child: ListTile(
-                  leading: CircleAvatar(
-                    radius: 30,
-                    child: Padding(
-                        padding: EdgeInsets.all(6),
-                        child: FittedBox(
-                            child: Text('₹ ${transaction[index].amount}'))),
-                  ),
-                  title: Text(
-                    transaction[index].title,
-                    style: Theme.of(context).appBarTheme.textTheme!.title,
-                  ),
-                  subtitle:
-                      Text(DateFormat.yMMMd().format(transaction[index].date)),
-                  trailing: IconButton(
-                    icon: Icon(Icons.delete),
-                    color: Theme.of(context).errorColor,
-                    onPressed: () => deleteTx(transaction[index].id),
-                  ),
+      itemBuilder: (ctx, index) {
+        return Card(
+            elevation: 2,
+            margin: EdgeInsets.symmetric(vertical: 7, horizontal: 10),
+            child: ListTile(
+                leading: CircleAvatar(
+                  radius: 30,
+                  child: Padding(
+                      padding: EdgeInsets.all(6),
+                      child: FittedBox(
+                          child: Text('₹ ${transaction[index].amount}'))),
                 ),
-              );
-            },
-            itemCount: transaction.length,
-            // children: transaction.map((tx) {}).toList(),
-          );
+                title: Text(
+                  transaction[index].title,
+                  style: Theme
+                      .of(context)
+                      .appBarTheme
+                      .textTheme!
+                      .title,
+                ),
+                subtitle:
+                Text(DateFormat.yMMMd().format(transaction[index].date)),
+                trailing: MediaQuery
+                    .of(context)
+                    .size
+                    .width > 460
+                    ? FlatButton.icon(
+                  icon: Icon(Icons.delete),
+                  textColor: Theme
+                      .of(context)
+                      .errorColor,
+                  onPressed: () => deleteTx(transaction[index].id),
+                    label: Text(
+                  'Delete',
+                ))
+                : IconButton(
+            icon: Icon(Icons.delete),
+        color: Theme.of(context).errorColor,
+        onPressed: () => deleteTx(transaction[index].id),
+        ),
+        ),
+        );
+      },
+      itemCount: transaction.length,
+      // children: transaction.map((tx) {}).toList(),
+    );
   }
 }
